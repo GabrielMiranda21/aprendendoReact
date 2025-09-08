28f56160b6910be4c821175ebafa7c019f902d42
GabrielMiranda21 <gabrielmirand204@gmail.com>
2025-09-08 16:47:36 -0300

    Header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import style from './Header.module.css';
+import ignite from '../assets/ignite.svg';
 
 export function Header(){
     return (
         <header className={style.header}>
-            <img src="#" alt="logo_ignite" />
+            <img src={ignite} alt="logo_ignite" />
             <strong>Ignite</strong>
         </header>
     )
